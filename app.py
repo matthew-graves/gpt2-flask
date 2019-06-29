51ac20018f8e2e1262c9f61da14c21e3da9c73cd
Doug Alexander <trippedoutfish@gmail.com>
2019-06-28 20:26:58 -0700

    Hopefully abstracting towards a queue

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, flash
 from config import Config
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-from web_model_generator import run_model
+from web_model_generator import init_model, generate_response
 
 DEBUG = True
 app = Flask(__name__)
 app.config.from_object(Config)
+
+sess, context, saver, output, enc = init_model()
 
 class ReusableForm(Form):
     prompt = TextField('', validators=[validators.required()])
@@ -19,7 +21,7 @@ class ReusableForm(Form):
             print(prompt)
 
             if form.validate():
-                result_text = run_model(prompt)
+                result_text =  generate_response(prompt, sess, context, saver, enc, output)
                 result_text = prompt + result_text
             else:
                 flash('All Fields Are Required')
--- web_model_generator.py
+++ web_model_generator.py
@@ -8,6 +8,83 @@ import tensorflow as tf
 
 import model, sample, encoder
 
+def init_model(model_name='345M',seed=None,nsamples=1,batch_size=1,length=None,temperature=1,top_k=40,models_dir='models'):
+    #region Explanation
+    """
+    Interactively run the model
+    :model_name=345 : String, which model to use
+    :seed=None : Integer seed for random number generators, fix seed to reproduce
+     results
+    :nsamples=1 : Number of samples to return total
+    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
+    :length=None : Number of tokens in generated text, if None (default), is
+     determined by model hyperparameters
+    :temperature=1 : Float value controlling randomness in boltzmann
+     distribution. Lower temperature results in less random completions. As the
+     temperature approaches zero, the model will become deterministic and
+     repetitive. Higher temperature results in more random completions.
+    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
+     considered for each step (token), resulting in deterministic completions,
+     while 40 means 40 words are considered at each step. 0 (default) is a
+     special setting meaning no restrictions. 40 generally is a good value.
+     :models_dir : path to parent folder containing model subfolders
+     (i.e. contains the <model_name> folder)     
+    """
+    #endregion
+
+    models_dir = os.path.expanduser(os.path.expandvars(models_dir))
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
+    enc = encoder.get_encoder(model_name, models_dir)
+    hparams = model.default_hparams()
+    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if length is None:
+        length = hparams.n_ctx // 2
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    sess = tf.compat.v1.Session() 
+    with sess.as_default():
+        context = tf.compat.v1.placeholder(tf.int32, [batch_size, None])
+        np.random.seed(seed)
+        tf.compat.v1.set_random_seed(seed)
+        output = sample.sample_sequence(
+            hparams=hparams, length=length,
+            context=context,
+            batch_size=batch_size,
+            temperature=temperature, top_k=top_k
+        )   
+        saver = tf.compat.v1.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
+        saver.restore(sess, ckpt)
+
+    return sess, context, saver, output, enc
+
+def generate_response(prompt_text, sess, context, saver, enc, output ):
+    context_tokens = enc.encode(prompt_text)
+    generated = 0
+    full_text = ''
+    
+    with sess.as_default():
+        nsamples = 1
+        batch_size = 1
+        for _ in range(nsamples // batch_size):
+            out = sess.run(output, feed_dict={
+                context: [context_tokens for _ in range(batch_size)]
+            })[:, len(context_tokens):]
+            for i in range(batch_size):
+                generated += 1
+                text = enc.decode(out[i])
+                full_text += text
+                full_text = "<br />". join(full_text.split("\n"))
+                full_text = full_text.split('<|endoftext|>', 1)[0]
+        return full_text    
+
+
 def interact_model(
     model_name='345M',
     seed=None,
